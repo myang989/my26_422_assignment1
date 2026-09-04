0a01812e79faef0923374eabc23ad4f7e9efe1b9
myang989 <142959190+myang989@users.noreply.github.com>
2026-09-03 21:00:36 -0400

    testing only print ip adresses not host name works

--- ping_rtt.sh
+++ ping_rtt.sh
@@ -2,6 +2,9 @@
 {
     read -r _header
     while IFS=, read -r col1 _rest; do
+        if ! [[ $col1 =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+            continue
+        fi
         echo "read $col1"
     done
 } < listed_iperf3_servers.csv
