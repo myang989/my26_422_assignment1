5704e9a2d4c15ae40516d63f37b276652f6caec7
myang989 <142959190+myang989@users.noreply.github.com>
2026-09-03 23:17:10 -0400

    succesfully generated graph

--- ping_rtt.py
+++ ping_rtt.py
@@ -1,13 +1,14 @@
 import csv
 import re
-from geopy.geocoders import Nominatim
+import requests
+import time
+from collections import defaultdict
+import matplotlib.pyplot as plt
+from geopy.distance import geodesic
 
-
-# initailize geolocator
-geolocator = Nominatim(user_agent="ping_rtt_resolver")
-
-# map for ip address to geo coord
-ip_to_coord = {}
+# map for ip address to geo coord / times
+# list index 0 stores the coord (lat,lon), index 1 has (min, rtt, max
+ip_map = defaultdict(list)
 
 with open('listed_iperf3_servers.csv', newline='') as f:
     reader = csv.DictReader(f)
@@ -16,9 +17,46 @@ with open('listed_iperf3_servers.csv', newline='') as f:
             # skip if not ip
             continue
         # if we have an ip, find its coords
-        location = geolocator.geocode(f"{row['COUNTRY']}, {row['SITE']}")
+        print(f"Mapping {row['IP/HOST']}")
+        r = requests.get(f"http://ip-api.com/json/{row['IP/HOST']}", timeout=5)
+        data = r.json()
+        lat = data['lat']
+        lon = data['lon']
+        time.sleep(1.5)
         # add ip and coords to dict
-        ip_to_coord[row['IP/HOST']] = (location.latitude, location.longitude)
+        ip_map[row['IP/HOST']].append((lat, lon))
 
-for i in ip_to_coord.items():
-    print(i)
+# read the bash script results in output.txt to add times to map
+with open('output.txt') as f:
+    for line in f:
+        ip, min_val, avg_val, max_val = re.match(
+            r'(\S+) min=([\d.]+)ms avg=([\d.]+)ms max=([\d.]+)ms', line
+        ).groups()
+        ip_map[ip].append((min_val, avg_val, max_val))
+        
+# now with the map we can construct the scatter plot
+# my public IP is:
+my_public_ip = requests.get('https://ifconfig.me').text.strip()
+# lat lon is:
+r = requests.get(f"http://ip-api.com/json/{my_public_ip}", timeout=5)
+data = r.json()
+lat_me = data['lat']
+lon_me = data['lon']
+
+
+for ip, val in ip_map.items():
+    lat, lon = val[0]
+    distance_km = geodesic((lat_me, lon_me), (lat, lon)).km
+    val.append(distance_km)
+
+# list index 0 stores the coord (lat,lon), index 1 has (min, rtt, max), index 2 has distance from me
+distances = [val[2] for _, val in ip_map.items()]
+rtt = [float(val[1][1]) for _, val in ip_map.items()]
+
+plt.scatter(distances, rtt)
+plt.xlabel('Distance (km) from my location')
+plt.ylabel('rtt (avg of 5 pings)')
+plt.title('Plot showing distance vs rtt for iperf3serverlist ip addresses')
+plt.yticks(range(int(min(rtt)), int(max(rtt)) + 1, 50))  # tick every 50ms
+plt.figure(figsize=(10, 8))
+plt.show()
